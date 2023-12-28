216cafb94198ffdf30daaa41ec1513a273112012
BlumaDeutsch <blimy1219@gmail.com>
2023-12-28 11:27:55 +0200

    something

--- routes/projects.routes.js
+++ routes/projects.routes.js
@@ -1,19 +1,20 @@
 const express = require("express");
-const { createProject, editProject, deleteProject, findProjectById, addUser, removeUser, addTask, removeTask, editTask } = require("../controllers/project.controller");
+const { createProject, editProject, deleteProject, findProjectByIdReturnTasks, findProjectByIdReturnUsers, addUser, removeUser, addTask, removeTask, editTask } = require("../controllers/project.controller");
 const router = express.Router();
 const { auth } = require("../middlewares/auth");
 
 
 
-router.post("/createProject",auth(), createProject);
-router.patch("/editProject/:id",auth(), editProject);
-router.delete("/deleteProject/:id",auth(), deleteProject);
-router.get("/findProjectById/:id", findProjectById);
-router.post("/addUser",auth(), addUser);
-router.delete("/removeUser",auth(), removeUser);
-router.post("/addTask",auth(), addTask);
-router.delete("/removeTask",auth(), removeTask);
-router.patch("/editTask",auth(), editTask);
+router.post("/createProject", auth(), createProject);
+router.patch("/editProject/:id", auth(), editProject);
+router.delete("/deleteProject/:id", auth(), deleteProject);
+router.get("/findProjectByIdReturnTasks/:id", findProjectByIdReturnTasks);
+router.get("/findProjectByIdReturnUsers/:id", findProjectByIdReturnUsers);
+router.post("/addUser", auth(), addUser);
+router.delete("/removeUser", auth(), removeUser);
+router.post("/addTask", auth(), addTask);
+router.delete("/removeTask", auth(), removeTask);
+router.patch("/editTask", auth(), editTask);
 
 
 module.exports = router;
